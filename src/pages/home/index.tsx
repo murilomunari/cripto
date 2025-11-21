90cacf35f577a3bd99ec819dff27855c23f1f572
murilomunari <munarimurilo441@gmail.com>
2025-11-21 16:54:39 -0300

    feat: conexão com API de criptomoedas

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,32 +1,57 @@
-import { useState, type FormEvent } from 'react'
+import { useState, type FormEvent, useEffect } from 'react'
 import styles from './home.module.css'
-import { BsSearch} from 'react-icons/bs'
+import { BsSearch } from 'react-icons/bs'
 import { Link, useNavigate } from 'react-router-dom'
 
 
-export function Home () {
+export function Home() {
     const [input, setInput] = useState("");
+    const [coins, setCoins] = useState([]);
 
     const navigate = useNavigate();
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    async function getData() {
+        try {
+            const response = await fetch(
+                "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1"
+            );
+
+            const data = await response.json();
+            console.log(data);
+            setCoins(data);
+
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
 
     function handleSubmit(e: FormEvent) {
         e.preventDefault(); // para cancelar o carregamento automatico da pagina 
 
-        if(input === "") return alert("digite a moeda desejada");
+        if (input === "") return alert("digite a moeda desejada");
 
         navigate(`/datail/${input}`)
     }
 
-    return(
+    function handleGetMore() {
+        alert("funcionando")
+    }
+
+    return (
         <div className={styles.home}>
             <form className={styles.form} onSubmit={handleSubmit}>
-                <input type="text" 
-                        placeholder='Digite o nome da moeada... EX bitcoin'
-                        value={input}
-                        onChange={ (e) => setInput(e.target.value)}/>
+                <input type="text"
+                    placeholder='Digite o nome da moeada... EX bitcoin'
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)} />
 
                 <button type='submit'>
-                <BsSearch size={30} color='#FFF'/>
+                    <BsSearch size={30} color='#FFF' />
                 </button>
             </form>
 
@@ -43,36 +68,39 @@ export function Home () {
 
                 <tbody id='tbody'>
 
-                
-                <tr className={styles.tr}>
-                    <td className={styles.tdLabel} data-label="Moeda">
-                        <div>
-                            <Link to={"/detail/bitcoin"}>
-                                <span>Bitcoin</span> | BTC
-                            </Link>
-                        </div>
-                    </td>
 
-                    <td className={styles.tdLabel} data-label="Valor mercado">
-                        1T
-                    </td>
+                    <tr className={styles.tr}>
+                        <td className={styles.tdLabel} data-label="Moeda">
+                            <div>
+                                <Link to={"/detail/bitcoin"}>
+                                    <span>Bitcoin</span> | BTC
+                                </Link>
+                            </div>
+                        </td>
 
-                    <td className={styles.tdLabel} data-label="Preço">
-                        8.800
-                    </td>
+                        <td className={styles.tdLabel} data-label="Valor mercado">
+                            1T
+                        </td>
 
-                    <td className={styles.tdLabel} data-label="Volume">
-                        2B
-                    </td>
+                        <td className={styles.tdLabel} data-label="Preço">
+                            8.800
+                        </td>
 
-                    <td className={styles.tdProfit} data-label="Mudança 24h">
-                        <span>1.20</span>
-                    </td>
-                </tr>
-            </tbody>
-            
+                        <td className={styles.tdLabel} data-label="Volume">
+                            2B
+                        </td>
+
+                        <td className={styles.tdProfit} data-label="Mudança 24h">
+                            <span>1.20</span>
+                        </td>
+                    </tr>
+                </tbody>
+
             </table>
-            
+
+            <button className={styles.buttonMore} onClick={handleGetMore}>
+                Carregar mais
+            </button>
 
         </div>
     )
