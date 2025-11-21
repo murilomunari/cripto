c30ac1fbcc33407ab7fb3d03def513cad15f3d1a
murilomunari <munarimurilo441@gmail.com>
2025-11-21 12:06:24 -0300

    feat: criado a tabela e o formulario da moedas e criando funcionalidades da pagina home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,78 @@
+import { useState, type FormEvent } from 'react'
 import styles from './home.module.css'
+import { BsSearch} from 'react-icons/bs'
+import { Link, useNavigate } from 'react-router-dom'
 
 
 export function Home () {
+    const [input, setInput] = useState("");
+
+    const navigate = useNavigate();
+
+    function handleSubmit(e: FormEvent) {
+        e.preventDefault(); // para cancelar o carregamento automatico da pagina 
+
+        if(input === "") return alert("digite a moeda desejada");
+
+        navigate(`/datail/${input}`)
+    }
+
     return(
-        <div>
-            <h1>Pagina Home</h1>
+        <div className={styles.home}>
+            <form className={styles.form} onSubmit={handleSubmit}>
+                <input type="text" 
+                        placeholder='Digite o nome da moeada... EX bitcoin'
+                        value={input}
+                        onChange={ (e) => setInput(e.target.value)}/>
+
+                <button type='submit'>
+                <BsSearch size={30} color='#FFF'/>
+                </button>
+            </form>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th scope='col'>Moeda</th>
+                        <th scope='col'>Valor de mercado</th>
+                        <th scope='col'>Preço</th>
+                        <th scope='col'>Volume</th>
+                        <th scope='col'>Mudança 24h</th>
+                    </tr>
+                </thead>
+
+                <tbody id='tbody'>
+
+                
+                <tr className={styles.tr}>
+                    <td className={styles.tdLabel} data-label="Moeda">
+                        <div>
+                            <Link to={"/detail/bitcoin"}>
+                                <span>Bitcoin</span> | BTC
+                            </Link>
+                        </div>
+                    </td>
+
+                    <td className={styles.tdLabel} data-label="Valor mercado">
+                        1T
+                    </td>
+
+                    <td className={styles.tdLabel} data-label="Preço">
+                        8.800
+                    </td>
+
+                    <td className={styles.tdLabel} data-label="Volume">
+                        2B
+                    </td>
+
+                    <td className={styles.tdProfit} data-label="Mudança 24h">
+                        <span>1.20</span>
+                    </td>
+                </tr>
+            </tbody>
+            
+            </table>
+            
 
         </div>
     )
